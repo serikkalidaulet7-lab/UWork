66723cfc4ef01fe6be2df442dda4eabed8f2dffd
mac <mac@macs-MacBook-Air.local>
2026-04-06 19:32:14 +0200

    5commit

--- UWork/UWork/urls.py
+++ UWork/UWork/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
+
+def health(request):
+    return HttpResponse("OK")
+
+
 urlpatterns = [
+    path("health/", health),
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
 ]
